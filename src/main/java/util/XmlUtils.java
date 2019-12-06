1c177acd14d6f298e8fabfe9e7fb8c13d6d3f0f7
gx <fairwinds0099@gmail.com>
2019-12-06 12:56:06 -0600

    refactored utils for spring injection

---
+++ src/main/java/util/XmlUtils.java
@@ -0,0 +1,27 @@
+package util;
+
+import model.Person;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class XmlUtils {
+
+    public static void serializeToXml(Class<?> T, Object object, String filePath) {
+
+        JAXBContext context = null;
+        try {
+            context = JAXBContext.newInstance(T);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.marshal(object, new FileOutputStream(filePath));
+        } catch (JAXBException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
